c85a91110e612212b94216d609efe2b21d5e5768
Sumit Jaiswal <sj0269950@gmail.com>
2026-08-09 22:23:37 +0530

    Fix CORS error by allowing x-request-id header

--- backend/api/handlers.go
+++ backend/api/handlers.go
@@ -38,7 +38,7 @@ func SetupRoutes(hub *ws.Hub) *mux.Router {
 				w.Header().Set("Access-Control-Allow-Credentials", "true")
 			}
 			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With")
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With, X-Request-ID")
 			
 			if req.Method == http.MethodOptions {
 				w.WriteHeader(http.StatusOK)
